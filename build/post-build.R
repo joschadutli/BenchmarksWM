d020c5a59929b14f6600af384dfa8693260c5867
Andy J. Wills <andy@willslab.co.uk>
2022-01-11 14:52:22 +0000

    Removing non-ASCII from jarrold10 dataset

--- build/post-build.R
+++ build/post-build.R
@@ -5,10 +5,11 @@ library(stringi)
 
 ## jarrold10 dataset has non-ASCII characters in it, this is not allowed in an
 ## R package. The following code converts them to ASCII. As the 3rd line
-## shows, they now appear as a form of question mark
+## shows, they now appear as NA
 fnam <- "../pkg/data/jarrold10.rda" 
 load(fnam)
 jarrold10$response <- stri_trans_general(jarrold10$response, "latin-ascii")
+jarrold10$response <- iconv(jarrold10$response, from="UTF-8", to="ASCII")
 unique(jarrold10$response)
 save(jarrold10, file=fnam)
 
